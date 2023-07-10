743cb0b75bfe39da20466e9ad49b01daa7c5a4d1
JunYoung <108723143+DayTeaJun@users.noreply.github.com>
2023-07-10 13:42:57 +0900

    230710 이벤트 컴포넌트 스타일 패딩 추가

--- React/shoppingmall/src/routes/Event.jsx
+++ React/shoppingmall/src/routes/Event.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Event() {
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center", padding: "10px" }}>
       <h1>오늘의 이벤트</h1>
       <Outlet></Outlet>
     </div>
