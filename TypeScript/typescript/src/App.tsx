3541863a14a701b816d80d6a8e5dcf71b11e06da
DayTeaJun <gkfnck@gmail.com>
2023-09-06 23:54:58 +0900

    230906 타입스크립트 redux

--- TypeScript/typescript/src/App.tsx
+++ TypeScript/typescript/src/App.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "redux"; // Dispatch 타입을 리덕스에서 가져옴
+import { RootState } from "./index";
 
 // jsx 타입지정 (자동으로 타입지정 되어있긴함)
 let 박스: JSX.Element = <div></div>;
@@ -9,12 +12,27 @@ function App() {
   let [user, setUser] = useState("kim");
   // useState에 string 또는 number(또는 다른 타입들)를 넣고 싶을 때(Generic 문법 사용)
   let [user2, setUser2] = useState<string | number>("kim");
+  const 꺼내온거 = useSelector((state: RootState) => state);
+  const dispatch: Dispatch = useDispatch(); // state 수정 요청
 
   return (
-    <div>
-      <h4>하이</h4>
-      <Profile name="철수" age="20"></Profile>
-    </div>
+    <>
+      <div>
+        <h4>하이</h4>
+        <Profile name="철수" age="20"></Profile>
+      </div>
+      // redux에서 만든 state쓸 때
+      <div className="App">
+        {꺼내온거.count}
+        <button
+          onClick={() => {
+            dispatch({ type: "증가" });
+          }}
+        >
+          버튼
+        </button>
+      </div>
+    </>
   );
 }
 
--- TypeScript/typescript/src/index.tsx
+++ TypeScript/typescript/src/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+// redux 타입스크립트
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+interface Counter {
+  count: number;
+}
+
+const 초기값: Counter = { count: 0 };
+// 위에 지정해서 자동지정해줌(state), action (dispatch() 날릴때 넣는 파라미터)
+function reducer(state = 초기값, action: { type: string }) {
+  if (action.type === "증가") {
+    return { count: state.count + 1 };
+  } else if (action.type === "감소") {
+    return { count: state.count - 1 };
+  } else {
+    return 초기값;
+  }
+}
+
+const store = createStore(reducer);
+
+// store의 타입 미리 export 해두기 (리덕스 공식문서 내용)
+export type RootState = ReturnType<typeof store.getState>;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
