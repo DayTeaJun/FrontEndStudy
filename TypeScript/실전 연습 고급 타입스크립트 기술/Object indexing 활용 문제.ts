b891cbfd8e2d6267f561235a18110b04a565fe1b
DayTeaJun <gkfnck@gmail.com>
2023-10-04 17:53:14 +0900

    231004 Object indexing 종합 연습문제

--- TypeScript/실전 연습 고급 타입스크립트 기술/Object indexing 활용 문제.ts
+++ TypeScript/실전 연습 고급 타입스크립트 기술/Object indexing 활용 문제.ts
@@ -46,3 +46,30 @@ type EventType = Event["type"];
 type EventType2 = Event["event"];
 
 type tests = [Expect<Equal<EventType, "click" | "focus" | "keydown">>];
+
+// indexing 통합 연습문제
+export type Expect<T extends true> = T;
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+  T
+>() => T extends Y ? 1 : 2
+  ? true
+  : false;
+
+export type Event =
+  | {
+      type: "click";
+      event: MouseEvent;
+    }
+  | {
+      type: "focus";
+      event: FocusEvent;
+    }
+  | {
+      type: "keydown";
+      event: KeyboardEvent;
+    };
+
+type EventType = Exclude<Event, { type: "keydown" }>;
+type EventType2 = EventType["type"];
+
+type tests = [Expect<Equal<EventType2, "click" | "focus">>];
