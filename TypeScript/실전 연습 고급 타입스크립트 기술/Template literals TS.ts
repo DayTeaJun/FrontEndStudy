d51ab18ace7eb4bc317151e4ea0df8459176fa1f
DayTeaJun <gkfnck@gmail.com>
2023-10-05 19:18:32 +0900

    231005 Template literals Extract

--- TypeScript/실전 연습 고급 타입스크립트 기술/Template literals TS.ts
+++ TypeScript/실전 연습 고급 타입스크립트 기술/Template literals TS.ts
@@ -11,3 +11,13 @@ makeUrl("/users");
 // 위 타입의 템플릿 리터럴에서 정의한 대로 넘기지 않으면 에러발생
 makeUrl("users/1");
 // string을 받는 함수를 안전하게 호출하는 것이 가능.
+
+// Extract 유틸리티를 이용하여, 뽑아낼 타입, 패턴을 넣으면 됨.
+import { Equal, Expect } from "../../helper";
+
+type Routes = "/users" | "/users/:id" | "/products" | "/products/:id";
+
+// 뽑아낼 패턴을 넣어 "/users/:id" | "/products/:id" 만 추출하게 함.
+type DynamicRoutes = Extract<Routes, `/${string}/:id`>;
+
+type tests = [Expect<Equal<DynamicRoutes, "/users/:id" | "/products/:id">>];
