d599609678762478c599b85b0ac8f958f0f9236b
DayTeaJun <gkfnck@gmail.com>
2023-10-06 19:23:16 +0900

    231006 Object key, value를 string Union 으로 변형

--- TypeScript/실전 연습 고급 타입스크립트 기술/Object 타입 변형.ts
+++ TypeScript/실전 연습 고급 타입스크립트 기술/Object 타입 변형.ts
@@ -147,3 +147,20 @@ type tests = [
     >
   >
 ];
+
+// Object Key, value를 string Union으로 변형
+interface FruitMap {
+  apple: "red";
+  banana: "yellow";
+  orange: "orange";
+}
+
+type TransformedFruit = {
+  [K in keyof FruitMap]: `${K}:${FruitMap[K]}`;
+}[keyof FruitMap];
+
+type tests = [
+  Expect<
+    Equal<TransformedFruit, "apple:red" | "banana:yellow" | "orange:orange">
+  >
+];
