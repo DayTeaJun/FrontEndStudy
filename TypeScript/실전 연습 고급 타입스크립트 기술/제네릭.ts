98aa8831a6faf3fd6b9258ed99e2591a120006e8
DayTeaJun <gkfnck@gmail.com>
2023-10-10 20:49:38 +0900

    231010 제네릭 타입 제한

--- TypeScript/실전 연습 고급 타입스크립트 기술/제네릭.ts
+++ TypeScript/실전 연습 고급 타입스크립트 기술/제네릭.ts
@@ -21,3 +21,22 @@ const one = returnWhatIPassIn(1);
 const matt = returnWhatIPassIn("matt");
 // 제네릭의 리턴은 받은 인자 그 자체로 number도 string도 아닌 1 "matt" 이다.
 type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, "matt">>];
+
+// 위처럼 제네릭은 타입 파리미터에 따라 계속해서 값이 변경되는데, 이걸 제한해 줄 수 있다.
+export const returnWhatIPassIn = <T extends string>(t: T) => t;
+// extends는 타입 파라미터의 타입을 제한해 줄 수 있다.
+const a = returnWhatIPassIn("a");
+
+type test1 = Expect<Equal<typeof a, "a">>;
+
+// 아래는 아규먼트(인수)가 위의 제네릭에서 extends 로 string으로 제한을 두었기 때문에 에러가 난다.
+// @ts-expect-error
+returnWhatIPassIn(1);
+
+// @ts-expect-error
+returnWhatIPassIn(true);
+
+// @ts-expect-error
+returnWhatIPassIn({
+  foo: "bar",
+});
