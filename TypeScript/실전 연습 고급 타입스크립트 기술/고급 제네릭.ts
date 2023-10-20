e7fc9878e97e07b9e8c6b6cd155129cdf3a81e77
DayTeaJun <gkfnck@gmail.com>
2023-10-20 19:40:53 +0900

    고급 제네릭: Currying

--- TypeScript/실전 연습 고급 타입스크립트 기술/고급 제네릭.ts
+++ TypeScript/실전 연습 고급 타입스크립트 기술/고급 제네릭.ts
@@ -1,3 +1,4 @@
+// 제네릭과 타입 제한
 // 들어오는 타입파라미터에 따라 타입을 제한
 type GreetingType<TGreeting> = TGreeting extends "goodbye"
   ? "hello"
@@ -27,3 +28,15 @@ it("Should return hello when goodbye is passed in", () => {
 
   expect(result).toEqual("hello");
 });
+
+// Curry : 커링은 하나 이상의 매개변수를 갖는 함수를 부분적으로 나누어 각각 단일 매개변수를 갖는 함수로 설정하는 기법
+
+// non-curried : 함수 실행 시 파라미터가 모자라도 문제 없이 실행이 가능함
+// ? 함수 정의 : func(a, b, c)
+// ? 함수 실행 : func(a)
+// ? 실행 결과 : func(a, undefined, undefined)
+
+// curried : 함수가 인수를 전부 받을 때까지 실행을 보류함.
+// ? 함수 정의 : func(a, b, c)
+// ? 함수 실행 : func(a)
+// ? 실행 결과 : func(a)상태에서 b 함수 입력 대기
