fd26c6f41194310ccc16dbcf9b132e4bb0666120
DayTeaJun <gkfnck@gmail.com>
2023-10-29 23:19:00 +0900

    Type Arguments: Object Key

--- TypeScript/실전 연습 고급 타입스크립트 기술/Type Arguments.ts
+++ TypeScript/실전 연습 고급 타입스크립트 기술/Type Arguments.ts
@@ -301,3 +301,26 @@ it("Should let you clone the cache using a transform function", () => {
 
   type tests = [Expect<Equal<typeof a, string | undefined>>];
 });
+
+// Object Key
+const getValue = <TObj, TKey extends keyof TObj>(obj: TObj, key: TKey) => {
+  return obj[key];
+};
+
+const obj = {
+  a: 1,
+  b: "some-string",
+  c: true,
+};
+
+const numberResult = getValue(obj, "a");
+const stringResult = getValue(obj, "b");
+const booleanResult = getValue(obj, "c");
+
+type tests = [
+  Expect<Equal<typeof numberResult, number>>,
+  Expect<Equal<typeof stringResult, string>>,
+  Expect<Equal<typeof booleanResult, boolean>>
+];
+
+export {};
