2593b1d24693fd77b96d7347d63a6c44f60016d8
DayTeaJun <gkfnck@gmail.com>
2023-11-01 01:38:27 +0900

    tsx 실습: 코드 및 주석 보충설명

--- React/ReactTypeScript/ts-react-실습/lecture/NumberBaseball.tsx
+++ React/ReactTypeScript/ts-react-실습/lecture/NumberBaseball.tsx
@@ -8,6 +8,7 @@ const getNumbers = () => {
   const array: number[] = [];
   for (let i = 0; i < 4; i += 1) {
     const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
   }
   return array;
 };
@@ -29,32 +30,20 @@ const NumberBaseball = () => {
   const inputEl = useRef<HTMLInputElement>(null);
 
   // useCallback으로 나누어 따로 사용하는 경우
-  const onSubmitForm = useCallback<(e: React.FormEvent) => void>((e) => {
-    e.preventDefault();
-    const input = inputEl.current;
-    if (value === answer.join("")) {
-      setTries((t) => [
-        ...t,
-        {
-          try: value,
-          result: "홈런!",
-        },
-      ]);
-      setResult("홈런!");
-      alert("게임을 다시 실행합니다.");
-      setValue("");
-      setAnswer(getNumbers());
-      setTries([]);
-      if (input) {
-        input.focus();
-      }
-    } else {
-      const answerArray = value.split("").map((v) => parseInt(v));
-      let strike = 0;
-      let ball = 0;
-      if (tries.length >= 9) {
-        setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(",")}였습니다.`);
-        alert("게임을 다시 시작합니다");
+  const onSubmitForm = useCallback<(e: React.FormEvent) => void>(
+    (e) => {
+      e.preventDefault();
+      const input = inputEl.current;
+      if (value === answer.join("")) {
+        setTries((t) => [
+          ...t,
+          {
+            try: value,
+            result: "홈런!",
+          },
+        ]);
+        setResult("홈런!");
+        alert("게임을 다시 실행합니다.");
         setValue("");
         setAnswer(getNumbers());
         setTries([]);
@@ -62,31 +51,50 @@ const NumberBaseball = () => {
           input.focus();
         }
       } else {
-        console.log("답은", answer.join(""));
-        for (let i = 0; i < 4; i += 1) {
-          if (answerArray[i] === answer[i]) {
-            console.log("strike", answerArray[i], answer[i]);
-            strike += 1;
-            // includes 는 ex2016년 기능으로 tsconfig에 추가
-          } else if (answer.includes(answerArray[i])) {
-            console.log("ball", answerArray[i], answer.indexOf(answerArray[i]));
-            ball += 1;
+        const answerArray = value.split("").map((v) => parseInt(v));
+        let strike = 0;
+        let ball = 0;
+        if (tries.length >= 9) {
+          setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(",")}였습니다.`);
+          alert("게임을 다시 시작합니다");
+          setValue("");
+          setAnswer(getNumbers());
+          setTries([]);
+          if (input) {
+            input.focus();
+          }
+        } else {
+          console.log("답은", answer.join(""));
+          for (let i = 0; i < 4; i += 1) {
+            if (answerArray[i] === answer[i]) {
+              console.log("strike", answerArray[i], answer[i]);
+              strike += 1;
+              // includes 는 ex2016년 기능으로 tsconfig에 추가
+            } else if (answer.includes(answerArray[i])) {
+              console.log(
+                "ball",
+                answerArray[i],
+                answer.indexOf(answerArray[i])
+              );
+              ball += 1;
+            }
+          }
+          setTries((t) => [
+            ...t,
+            {
+              try: value,
+              result: `${strike} 스트라이크, ${ball} 볼 입니다.`,
+            },
+          ]);
+          setValue("");
+          if (input) {
+            input.focus();
           }
         }
-        setTries((t) => [
-          ...t,
-          {
-            try: value,
-            result: `${strike} 스트라이크, ${ball} 볼 입니다.`,
-          },
-        ]);
-        setValue("");
-        if (input) {
-          input.focus();
-        }
       }
-    }
-  }, []);
+    },
+    [value, answer]
+  );
 
   return (
     <>
@@ -96,10 +104,17 @@ const NumberBaseball = () => {
           ref={inputEl}
           maxLength={4}
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          // 함수 분리하지 않은 경우는 vscode에서 타입추론 해줌
+          // onChange={(e) => setValue(e.target.value)}
+          // useCallback 사용시 타입 추론 안해줘서 타입 적어야함
+          onChange={useCallback(
+            (e: React.ChangeEvent<HTMLInputElement>) =>
+              setValue(e.target.value),
+            []
+          )}
         />
+        <button>입력!</button>
       </form>
-      <button>입력!</button>
       <div>시도 : {tries.length}</div>
       <ul>
         {tries.map((v, i) => (
