d6e35bb5e020c6f44ac39354c0962f6d659442de
DayTeaJun <gkfnck@gmail.com>
2023-12-22 21:36:49 +0900

    Firebase : 네비게이션 추기

--- JavaScript/Firebase/my-diary/src/components/Nav.js
+++ JavaScript/Firebase/my-diary/src/components/Nav.js
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
 import styles from './Nav.module.css';
 
 export default function Nav() {
-  return <nav className={styles.nav}></nav>;
+  return (
+    <nav className={styles.nav}>
+      <h1 className={styles.tlt}>일기</h1>
+      <ul className={styles.list_nav}>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
+        <li>
+          <Link to="/signup">회원가입</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
