72452535cd37369431cb5d5a313495b8ff2c6fa9
DayTeaJun <gkfnck@gmail.com>
2023-12-26 21:02:49 +0900

    Firebase : context 유저정보 관리 및 회원가입 설정

--- JavaScript/Firebase/my-diary/src/context/AuthContext.js
+++ JavaScript/Firebase/my-diary/src/context/AuthContext.js
@@ -6,6 +6,10 @@ const AuthContext = createContext();
 // Reducer, 객체 데이터 다룰 때 사용
 const authReducer = (state, action) => {
   switch (action.type) {
+    // 회원가입, 로그인 하나로 통합
+    case 'login':
+      // ...state(기존 유저 정보), action.payload로 받아온 유저정보를 합침
+      return { ...state, user: action.payload };
     default:
       return state;
   }
@@ -13,13 +17,19 @@ const authReducer = (state, action) => {
 
 // context 를 구독할 컴포넌트의 묶음 범위 설정
 const AuthContextProvider = ({ children }) => {
-  // 유저 정보 관리
+  // 유저 정보 관리 (state 유저 정보, dispatch는 authReducer 호출 state 업데이트)
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
 
+  console.log(state);
+
   // value 값을 children 에게 공유
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContext, AuthContextProvider };
--- JavaScript/Firebase/my-diary/src/hooks/useSignup.js
+++ JavaScript/Firebase/my-diary/src/hooks/useSignup.js
@@ -1,12 +1,15 @@
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { useState } from 'react';
 import { appAuth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
   // 에러 정보 저장
   const [error, setError] = useState(null);
   // 현재 서버와 통신 상태 저장
   const [isPending, setIsPending] = useState(false);
+  // 전역 공유적인 컨텍스트 받기
+  const { dispatch } = useAuthContext();
 
   const signup = (email, password, displayName) => {
     setError(null); // 아직 에러 없음
@@ -27,10 +30,9 @@ export const useSignup = () => {
         // appAuth.currentUser 는 현재 로그인한 유저의 정보
         updateProfile(appAuth.currentUser, { displayName })
           .then(() => {
-            setError(() => {
-              setError(null);
-              setIsPending(false);
-            });
+            dispatch({ type: 'login', payload: user });
+            setError(null);
+            setIsPending(false);
           })
           .catch((err) => {
             setError(err.message);
