d5529f9fd3f7fa79963b2d4fe6094c5d15f68b8a
DayTeaJun <gkfnck@gmail.com>
2023-12-29 19:12:09 +0900

    Firebase : 빌드 및 배포 완

--- JavaScript/Firebase/my-diary/src/components/Nav.js
+++ JavaScript/Firebase/my-diary/src/components/Nav.js
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
-import styles from "./Nav.module.css";
-import { useLogout } from "../hooks/useLogout";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { Link } from 'react-router-dom';
+import styles from './Nav.module.css';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Nav() {
   const { logout } = useLogout();
@@ -9,7 +9,7 @@ export default function Nav() {
 
   return (
     <nav className={styles.nav}>
-      <h1 className={styles.tlt}>일기</h1>
+      <h1 className={styles.tlt}>비밀일기</h1>
       <ul className={styles.list_nav}>
         {!user && (
           <>
