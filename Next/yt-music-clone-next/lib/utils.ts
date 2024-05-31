7ebf10828389aef09ce7c2ef0377ed274b786fe5
DayTeaJun <gkfnck@gmail.com>
2024-05-31 23:08:19 +0900

    Next YT Music dummyData add

--- Next/yt-music-clone-next/lib/utils.ts
+++ Next/yt-music-clone-next/lib/utils.ts
@@ -8,3 +8,9 @@ export function cn(...inputs: ClassValue[]) {
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 // ms의 시간이 흐른 뒤에 resolve
 // await sleep(1000)
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
