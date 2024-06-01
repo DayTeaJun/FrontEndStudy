3d0c6f5d12bf1d74b09767612508f58317482d32
DayTeaJun <gkfnck@gmail.com>
2024-06-01 11:23:38 +0900

    Next YT Music : searchSection

--- Next/yt-music-clone-next/components/Header.tsx
+++ Next/yt-music-clone-next/components/Header.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import React from 'react';
 import UserIcon from '@/components/UserIcon';
+import PagePadding from '@/components/PagePadding';
+import { FaChromecast } from 'react-icons/fa';
+import { FiSearch } from 'react-icons/fi';
 
 function Header({ children }) {
   return (
@@ -18,10 +21,28 @@ function Header({ children }) {
           <div className="absolute h-[400px] top-0 bg-gradient-to-t from-black opacity-40 w-full"></div>
         </div>
       </section>
-      <section>
-        <UserIcon />
+      {/* searchSection */}
+      <section className="sticky">
+        <PagePadding>
+          <div className="h-[64px] flex flex-row justify-between items-center">
+            <article className="h-[42px] min-w-[480px] flex flex-row items-center bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px]">
+              <div>
+                <FiSearch size={24} />
+              </div>
+              <input
+                className="h-full w-full bg-transparent"
+                type="text"
+                placeholder="노래, 앨범, 아티스트, 팟키스트 검색"
+              />
+            </article>
+            <article className="flex flex-row gap-6 items-center">
+              <FaChromecast size={26} />
+              <UserIcon />
+            </article>
+          </div>
+        </PagePadding>
       </section>
-      <section className="absolute">{children}</section>
+      <section className="relative">{children}</section>
     </header>
   );
 }
---
+++ Next/yt-music-clone-next/components/PagePadding.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function PagePadding({ children }) {
+  return (
+    // lg(1024px) 이 넘게 되면 px값을 100px 변화
+    <div className="mx-auto px-[10px] py-2 lg:px-[100px]">{children}</div>
+  );
+}
+
+export default PagePadding;
