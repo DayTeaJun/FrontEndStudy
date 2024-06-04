3f2b24aa15cb5fa00c1d2a53fb1b0f92256637ca
DayTeaJun <gkfnck@gmail.com>
2024-06-04 13:58:50 +0900

    Next YT Music : explore page - Category

--- Next/yt-music-clone-next/app/explore/layout.tsx
+++ Next/yt-music-clone-next/app/explore/layout.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
+import React from "react";
 
 // layout 다음 page 컴포넌트가 들어온다.
 function layout({ children }) {
-  return (
-    <div>
-      layout 먼저
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
 
 export default layout;
