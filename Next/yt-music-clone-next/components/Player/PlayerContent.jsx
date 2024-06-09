68165c448c23de4fe70fb896f6aad4fcfa8adc32
DayTeaJun <gkfnck@gmail.com>
2024-06-09 23:59:23 +0900

    Next YT Music : Player Play Song Slider UI

--- Next/yt-music-clone-next/components/Player/PlayerContent.jsx
+++ Next/yt-music-clone-next/components/Player/PlayerContent.jsx
@@ -1,12 +1,112 @@
 import React from 'react';
 import { Slider as PlayerSlider } from '@/components/ui/playerSlider';
+import { useAudio } from 'react-use';
+import {
+  IoPlaySkipBackSharp,
+  IoPlaySkipForwardSharp,
+  IoShuffle,
+  IoVolumeHighOutline,
+} from 'react-icons/io5';
+import { AiFillCaretUp, AiOutlinePause } from 'react-icons/ai';
+import usePlayerState from '@/hooks/usePlayerState';
+import { ClipLoader } from 'react-spinners';
+import { RiPlayFill } from 'react-icons/ri';
+import Image from 'next/image';
+import { RxLoop } from 'react-icons/rx';
 
 function PlayerContent() {
+  const { activeSong } = usePlayerState();
+
+  const [audio, state, controls, ref] = useAudio({
+    src: activeSong?.src,
+    autoPlay: false,
+  });
+
+  const isLoading = activeSong?.src && state.buffered?.length === 0;
+  // useAudio 내장함수 buffered
+  // 로딩 상태
+
+  const onClickPreBtn = () => {};
+  const onClickStartBtn = () => {
+    controls.play();
+  };
+  const onClickPauseBtn = () => {
+    controls.pause();
+  };
+  const onClickNextBtn = () => {};
+
   return (
     <div className="h-full w-full relative">
       <div className="absolute top-[-16px] w-full">
-        <PlayerSlider />
+        <PlayerSlider
+          className="w-full"
+          defaultValue={[0]}
+          value={[state.time]}
+          onValueChange={(value) => {
+            controls.seek(value);
+          }}
+        />
       </div>
+      {audio}
+      <section className="flex flex-row justify-between items-center w-full h-full px-2 lg:px-6">
+        <div className="h-full flex flex-row  items-center gap-1 lg:gap-8">
+          <IoPlaySkipBackSharp
+            size={24}
+            className="cursor-pointer"
+            onClick={onClickPreBtn}
+          />
+          {isLoading ? (
+            <ClipLoader color="#FFF" />
+          ) : state.playing ? (
+            <AiOutlinePause
+              size={40}
+              classNe="cursor-pointer"
+              onClick={onClickPauseBtn}
+            />
+          ) : (
+            <RiPlayFill
+              size={40}
+              classNe="cursor-pointer"
+              onClick={onClickStartBtn}
+            />
+          )}
+
+          <IoPlaySkipForwardSharp
+            size={24}
+            className="cursor-pointer"
+            onClick={onClickNextBtn}
+          />
+        </div>
+        <article>
+          <div className="flex flex-row gap-4 items-center">
+            <div className=" relative w-[40px] h-[40px] flex">
+              <Image
+                fill
+                className="object-cover"
+                src={activeSong?.imageSrc}
+                alt="img"
+              />
+            </div>
+            <div className="flex flex-col">
+              <div>{activeSong?.name}</div>
+              <div className=" text-neutral-500 text-[14px]">
+                {activeSong?.channel} - 조회수 7.8만회 - 좋아요 1.7천개
+              </div>
+            </div>
+            <div className="flex flex-row gap-2">
+              <div className=" flex-row gap-2 hidden lg:flex">
+                <IoVolumeHighOutline size={24} className=" cursor-pointer" />
+                <IoShuffle size={24} className=" cursor-pointer" />
+                <RxLoop size={24} className=" cursor-pointer" />
+              </div>
+              <div>
+                <AiFillCaretUp size={24} className=" cursor-pointer" />
+              </div>
+            </div>
+          </div>
+        </article>
+        <div></div>
+      </section>
     </div>
   );
 }
