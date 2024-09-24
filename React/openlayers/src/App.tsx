8e974c85d4169312266bc0b318319630e9949840
DayTeaJun <gkfnck@gmail.com>
2024-09-24 23:23:11 +0900

    Feat: Openlayers 날씨 지도 데이터 테스트

--- React/openlayers/src/App.tsx
+++ React/openlayers/src/App.tsx
@@ -1,76 +1,24 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'ol/ol.css';
 import { Map as OLMap, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import ImageLayer from 'ol/layer/Image';
-import ImageCanvasSource from 'ol/source/ImageCanvas';
 import VectorSource from 'ol/source/Vector';
 import { fromLonLat } from 'ol/proj';
 import Feature from 'ol/Feature';
 import GeoJSON from 'ol/format/GeoJSON';
 import Circle from 'ol/geom/Circle';
-import { Vector as VectorLayer, ImageStatic } from 'ol/layer';
+import { Vector as VectorLayer } from 'ol/layer';
 import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
-import { Draw, Modify, Snap } from 'ol/interaction';
 import { WindLayer } from 'ol-wind';
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoidTEwaW50IiwiYSI6InQtMnZvTkEifQ.c8mhXquPE7_xoB3P4Ag8cA';
 
 const WindMap = () => {
-  const mapboxMapRef = useRef(null); // Mapbox를 위한 참조
   const olMapRef = useRef(null); // OpenLayers를 위한 참조
-
-  // Mapbox 초기화
-  const initMapbox = () => {
-    const map = new mapboxgl.Map({
-      container: mapboxMapRef.current,
-      style: {
-        version: 8,
-        sources: {},
-        layers: [
-          {
-            id: 'background',
-            type: 'background',
-            layout: {
-              visibility: 'none',
-            },
-          },
-        ],
-      },
-      center: { lng: 105.7015, lat: 22.7602 },
-      zoom: 0,
-      antialias: true,
-    });
-
-    map.on('load', () => {
-      // Mapbox와 바람 레이어 관리
-      const source = new mapboxgl.TileSource({
-        tileSize: 256,
-        url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-        minZoom: 0,
-        maxZoom: 18,
-        wrapX: true,
-      });
-
-      let flag = 0;
-      map.on('click', () => {
-        flag = flag === 0 ? 1 : 0;
-        source.update(
-          {
-            url:
-              flag === 0
-                ? 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
-                : 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-          },
-          false
-        );
-      });
-    });
-  };
 
   // OpenLayers 초기화
   const initOpenLayers = () => {
@@ -150,14 +98,15 @@ const WindMap = () => {
   };
 
   useEffect(() => {
-    initMapbox();
     initOpenLayers();
   }, []);
 
   return (
     <>
-      <div ref={mapboxMapRef} style={{ width: '100vw', height: '50vh' }} />
-      <div ref={olMapRef} style={{ width: '100vw', height: '100vh' }} />
+      <div
+        ref={olMapRef}
+        style={{ display: 'flex', width: '100vw', height: '100vh' }}
+      ></div>
     </>
   );
 };
