ed7639f292b7601f8c3a4da1a564fd242754aa2a
DayTeaJun <gkfnck@gmail.com>
2025-02-18 23:33:34 +0900

    suppabase-next 2

--- Next/supabase-next-todo/app/todo-no-ris/page.tsx
+++ Next/supabase-next-todo/app/todo-no-ris/page.tsx
@@ -1,12 +1,18 @@
 import { sleep } from "@/lib/utils";
 import React from "react";
+import TodoContainer from "./components/TodoContainer";
 
 // 서버 클라이언트
 const page = async () => {
   console.log("api 호출 중 로딩 보이도록");
   await sleep(1500);
   console.log("api 로딩 완료 후 페이지 보이도록");
-  return <div>page</div>;
+  return (
+    <div>
+      page
+      <TodoContainer />
+    </div>
+  );
 };
 
 export default page;
