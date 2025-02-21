36ac19b51e7260ab5f248bb5aa34b7cf207d55b3
DayTeaJun <gkfnck@gmail.com>
2025-02-21 21:33:52 +0900

    supabase api 2

--- Next/supabase-next-todo/apis/todos-no-ris.ts
+++ Next/supabase-next-todo/apis/todos-no-ris.ts
@@ -11,3 +11,26 @@ export const getTodos = async () => {
 
   return result.data;
 };
+
+// 동일 id 조회
+export const getTodosById = async (id: number) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_ris")
+    .select("*")
+    .is("deleted_at", null)
+    .eq("id", id);
+  return result.data;
+};
+
+// 검색
+export const getTodosBySearch = async (terms: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_ris")
+    .select("*")
+    .is("deleted_at", null)
+    .ilike("content", `%${terms}%`) // 대소문자 구별 없이 검색
+    .order("id", { ascending: false }); // 최신 데이터 순서
+  return result.data;
+};
--- Next/supabase-next-todo/app/todo-no-ris/components/TodoContainer.tsx
+++ Next/supabase-next-todo/app/todo-no-ris/components/TodoContainer.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { getTodos } from "@/apis/todos-no-ris";
+import { getTodos, getTodosById, getTodosBySearch } from "@/apis/todos-no-ris";
 import React, { useEffect } from "react";
 
 function TodoContainer() {
   useEffect(() => {
     getTodos();
+    getTodosById(4);
+    getTodosBySearch("여기");
   }, []);
 
   return <div>TodoContainer</div>;
