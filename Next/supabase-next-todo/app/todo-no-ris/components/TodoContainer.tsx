207c32e0ab7abc2afacad59e471d052b2605bd4e
DayTeaJun <gkfnck@gmail.com>
2025-02-21 23:43:26 +0900

    supabase api hook get

--- Next/supabase-next-todo/app/todo-no-ris/components/TodoContainer.tsx
+++ Next/supabase-next-todo/app/todo-no-ris/components/TodoContainer.tsx
@@ -1,25 +1,21 @@
 "use client";
-import {
-  createTodos,
-  deleteTodoHard,
-  deleteTodoSoft,
-  getTodos,
-  getTodosById,
-  getTodosBySearch,
-  updateTodos,
-} from "@/apis/todos-no-ris";
+
 import React, { useEffect } from "react";
+import useTodosController from "../hooks/useTodosController";
 
 function TodoContainer() {
-  useEffect(() => {
-    getTodos();
-    getTodosById(4);
-    getTodosBySearch("여기");
-    // createTodos("next.js에서 todo를 생성함");
-    updateTodos(6, "todo 업데이트함");
-    deleteTodoSoft(7);
-    // deleteTodoHard(7);
-  }, []);
+  // useEffect(() => {
+  //   getTodos();
+  //   getTodosById(4);
+  //   getTodosBySearch("여기");
+  //   createTodos("next.js에서 todo를 생성함");
+  //   updateTodos(6, "todo 업데이트함");
+  //   deleteTodoSoft(7);
+  //   deleteTodoHard(7);
+  // }, []);
+
+  const { loading, todos } = useTodosController();
+  console.log(loading, todos);
 
   return <div>TodoContainer</div>;
 }
