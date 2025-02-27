0379554d3ca559ab7d155cc1fddaac618e8e1cbd
DayTeaJun <gkfnck@gmail.com>
2025-02-27 14:26:31 +0900

    Supabase Middleware cookie-counter

--- Next/supabase-next-todo/middleware.ts
+++ Next/supabase-next-todo/middleware.ts
@@ -1,10 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const COOKIE_COUNTER = "cookie-counter";
+
 // middleware 사용자가 로그인 등 인증 및 권한이 필요한 경우 아래 middleware에 거쳐 접근하게 됨
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
-  console.log("middleware 통과함");
+  // 브라우저 쿠키에 cookie-counter 키와 값 들을 저장
+  if (request.cookies.get(COOKIE_COUNTER)?.value) {
+    // cookie-counter request
+    const prev = request.cookies.get(COOKIE_COUNTER)?.value;
+
+    // cookie-counter response
+    response.cookies.set(COOKIE_COUNTER, `${Number(prev) + 1}`);
+  } else {
+    // 초기 cookie-counter가 없을경우 1 세팅
+    response.cookies.set(COOKIE_COUNTER, "1");
+  }
 
   return response;
 }
