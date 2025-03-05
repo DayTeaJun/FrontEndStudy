9529b491a167e1a9af030d95434124cdd7589d89
DayTeaJun <gkfnck@gmail.com>
2025-03-05 19:10:21 +0900

    Supabase OAuth google login

--- Next/supabase-next-todo/components/AuthUi.jsx
+++ Next/supabase-next-todo/components/AuthUi.jsx
@@ -4,19 +4,50 @@ import useHydrate from "@/hooks/useHydrate";
 import { createSupabaseBrowserClient } from "@/lib/client/supabase";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function AuthUi() {
+  const [user, setUser] = useState();
   const supabase = createSupabaseBrowserClient();
   const isMount = useHydrate();
+
+  const getUserInfo = async () => {
+    const result = await supabase.auth.getUser();
+    console.log(result);
+    if (result?.data?.user) setUser(result?.data?.user);
+  };
+
+  const handleLogout = async () => {
+    supabase.auth.signOut();
+    // 페이지 새로고침
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   // 서버사이드 렌더링으로, 마크업을 먼저 불러오기 때문에 ui가 깨지는 것을 가리기 위해, useEffect으로 (첫 렌더링 시 false -> true 변화하는 과정) 서버와 클라이언트가 렌더링이 끝난 뒤 보여주도록 함
   if (!isMount) return null;
 
   return (
-    <section className="w-full">
+    <section className="w-full p-10">
+      <div>{user ? `로그인 된 ${user?.email}` : "로그아웃"}</div>
+      <>
+        {user && (
+          <button
+            onClick={handleLogout}
+            type="button"
+            className="border-2 border-black"
+          >
+            로그아웃
+          </button>
+        )}
+      </>
       <div className="mx-auto max-w-[500px]">
         <Auth
+          // 로그인 끝난 후 페이지 이동
+          redirectTo={process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO}
           supabaseClient={supabase}
           appearance={{
             theme: ThemeSupa,
