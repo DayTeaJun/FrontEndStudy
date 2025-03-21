95de5733edf99fa3a53ce5d6d344580c63860af8
DayTeaJun <gkfnck@gmail.com>
2025-03-21 17:03:16 +0900

    NextJS serverAction 정리

--- Next/next-practice/app/api/users/route.ts
+++ Next/next-practice/app/api/users/route.ts
@@ -1,11 +1,24 @@
 import { NextResponse } from "next/server";
 
+const DB = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
+// route.ts 파일은 Http api 를 구현하는 파일
+// 서버에서 실행되는 코드로, 클라이언트에서 직접 접근할 수 없음
+// 웹에서 접근이 불가능하여 보안이 필요한 코드를 작성할 때 사용 (DB 접속 등)
+
 export async function GET(request: Request) {
+  // URLSearchParams : URL의 query string을 다루기 위한 인터페이스
+  const searchParams = new URL(request.url).searchParams;
+  // URLSearchParams.get() : 특정 key에 대한 값을 가져옴
+  const name = searchParams.get("name") as string;
+
+  // NextResponse.json() : JSON 형식의 응답을 반환
+  // DB.filter() : DB 배열에서 name이 일치하는 요소만 필터링하여 반환
   return NextResponse.json({
-    users: [
-      { id: 1, name: "Alice" },
-      { id: 2, name: "Bob" },
-      { id: 3, name: "Charlie" },
-    ],
+    users: DB.filter((user) => user.name.includes(name)),
   });
 }
---
+++ Next/next-practice/app/users/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { SearchUsers } from "../actions/user-actions";
+
+function UsersPage() {
+  const [users, setUsers] = useState([]);
+
+  //   fetch(), rest api 호출 방법
+  //   useEffect(() => {
+  //     fetch(`api/users?name=${"Alice"}`)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setUsers(data.users);
+  //       });
+  //   }, []);
+
+  // 별도 api 구현없이 함수 임포트
+  useEffect(() => {
+    SearchUsers("Alice").then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <main>
+      <h1>users</h1>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </main>
+  );
+}
+
+export default UsersPage;
