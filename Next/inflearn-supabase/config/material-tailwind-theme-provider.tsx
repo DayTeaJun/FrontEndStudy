f3c5f53b2d3137c9df3cb4063d970f3d5348f9e1
DayTeaJun <gkfnck@gmail.com>
2025-03-24 18:48:41 +0900

    NextJS supabase todo init setting (tailwind Mui)

---
+++ Next/inflearn-supabase/config/material-tailwind-theme-provider.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+// 아래 라이브러리는 클라이언트 컴포넌트에서 동작함.
+// 따라서 서버사이드에서는 사용할 수 없음.
+// 서버사이드 렌더링인 layout에서 바로 직접 사용하면 에러가 나기때문에, 이 파일을 만들어서 사용함.
+export { ThemeProvider } from "@material-tailwind/react";
