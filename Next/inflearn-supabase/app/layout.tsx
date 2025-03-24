afa9240e03016796c59fbf0f1ce8aab941d48580
DayTeaJun <gkfnck@gmail.com>
2025-03-25 00:04:55 +0900

    NextJS version change supabase

--- Next/inflearn-supabase/app/layout.tsx
+++ Next/inflearn-supabase/app/layout.tsx
@@ -13,20 +13,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ThemeProvider>
-      <html lang="en">
-        <head>
-          {/* font-awesome 라이브러리 (간단한 아이콘 ui)*/}
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-            integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-        </head>
-        <body>{children}</body>
-      </html>
-    </ThemeProvider>
+    <html lang="en">
+      <head>
+        {/* font-awesome 라이브러리 (간단한 아이콘 ui)*/}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
   );
 }
